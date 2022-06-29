ca22ef8d7620ec821f82d27a8136b490f87968ef
АндрейСолуянов <soluyanov.a@yandex.ru>
2022-06-29 11:15:06 +0300

    Устанавливаем npm i -D css-loader mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     // development ,production
     mode: 'development',
@@ -10,11 +11,34 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
+
+    module: {
+        rules: [
+            {
+                //
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+
+                    },
+                    'css-loader'
+                ],
+            },
+        ],
+    },
+
     plugins: [
         new HtmlWebpackPlugin(
             {
                 template: './src/index.html'
             }
-        )
+        ),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: '[name].css'
+
+        })
     ]
 }
